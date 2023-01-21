f11477198d6d264a4c99f5e7744c89472d5eb9c7
Tommaso Lo Piparo <72195663+Pippobaudoicon@users.noreply.github.com>
2023-01-22 00:27:01 +0100

    comments

--- wifi_password.py
+++ wifi_password.py
@@ -5,7 +5,7 @@ data = meta_data.decode('utf-8', errors="backslashreplace")
 data = data.split('\n')
 profiles = []
 for i in data:
-    # Change it into your language -- can be found by running "netsh wlan show profiles" on cmd
+    # Change it into your system language -- can be found by running "netsh wlan show profiles" on cmd
     if "Tutti i profili utente" in i:
         i = i.split(":")
         i = i[1]
@@ -21,7 +21,7 @@ for i in profiles:
         results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear'])
         results = results.decode('utf-8', errors="backslashreplace")
         results = results.split('\n')
-        # Change it into your language -- can be found by running "netsh wlan show profile [profile name] key=clear" on cmd
+        # Change it into your system language -- can be found by running "netsh wlan show profile [profile name] key=clear" on cmd
         results = [b.split(":")[1][1:-1] for b in results if "Contenuto chiave" in b]
 
         try:
