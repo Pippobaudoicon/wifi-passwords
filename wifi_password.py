b81deacb26a97dc49609c09e8572a0491680aa7f
Tommaso Lo Piparo <72195663+Pippobaudoicon@users.noreply.github.com>
2023-01-22 00:26:05 +0100

    Update wifi_password.py

--- wifi_password.py
+++ wifi_password.py
@@ -5,6 +5,7 @@ data = meta_data.decode('utf-8', errors="backslashreplace")
 data = data.split('\n')
 profiles = []
 for i in data:
+    # Change it into your language -- can be found by running "netsh wlan show profiles" on cmd
     if "Tutti i profili utente" in i:
         i = i.split(":")
         i = i[1]
@@ -20,13 +21,14 @@ for i in profiles:
         results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear'])
         results = results.decode('utf-8', errors="backslashreplace")
         results = results.split('\n')
+        # Change it into your language -- can be found by running "netsh wlan show profile [profile name] key=clear" on cmd
         results = [b.split(":")[1][1:-1] for b in results if "Contenuto chiave" in b]
 
         try:
             print("{:<30}| {:<}".format(i, results[0]))
         
         except IndexError:
-            print("{:<30}| {:<}".format(i, ""))
+            print("{:<30}| {:<}".format(i, "No Password"))
 
     except subprocess.CalledProcessError:
-        print("Encoding Error Occured")
+        print("Encoding Error")
